33624dfe8e85f81f37006f7eb59baae69617c467
Yukiko Nii <50726084+NIIY64308@users.noreply.github.com>
2024-06-30 23:43:10 +0900

    Delete Todo-Shared.js

--- Todo-Shared.js
+++
@@ -1,8 +0,0 @@
-// get due dates
-var getDate =  document.getElementsByClassName('entrydate');
-var getDateArr = [];
-
-window.addEventListener('click', function () {
-for (var i = 0; i < getDate.length; i++) {
-  getDateArr.push(getDate[i].innerHTML.slice(8));
-}});
